9d5ca55003fadbfa07fa8e34e005464bbe51d40d
Cristian Geronimo <crisalbertoaa20@gmail.com>
2024-07-10 23:32:48 -0300

    Primer commit, se genero el proyecto inicial con spring boot initializr

---
+++ src/main/java/com/aluralatam/topichubapirest/TopichubapirestApplication.java
@@ -0,0 +1,13 @@
+package com.aluralatam.topichubapirest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TopichubapirestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TopichubapirestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aluralatam/topichubapirest/TopichubapirestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aluralatam.topichubapirest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TopichubapirestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
